069259a431dcc146ecd4f96dd8f3c813dfdf915b
Crois0509 <crois0509@naver.com>
2025-01-17 20:16:43 +0900

    🔧[conf] 프로젝트 파일 생성

---
+++ TripLog/TripLog/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripLog
+//
+//  Created by 장상경 on 1/17/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
