ae3d0ed064ac9d56381ba0915f577ed6ce89c061
Crois0509 <crois0509@naver.com>
2025-01-20 19:27:14 +0900

    ✨[feat] 모달뷰 상태를 정의할 열거형 타입 선언

---
+++ TripLog/TripLog/Feat-Crois/ModalViewController/Model/ModalViewState.swift
@@ -0,0 +1,25 @@
+//
+//  ModalViewState.swift
+//  TripLog
+//
+//  Created by 장상경 on 1/20/25.
+//
+
+import Foundation
+
+enum ModalViewState {
+    case createNewCashBook
+    case createNewbudget
+    case editBudget
+    
+    var modalTitle: String {
+        switch self {
+        case .createNewCashBook:
+            return "새 가계부 만들기"
+        case .createNewbudget:
+            return "새 지출내역 추가하기"
+        case .editBudget:
+            return "지출내역 수정하기"
+        }
+    }
+}
