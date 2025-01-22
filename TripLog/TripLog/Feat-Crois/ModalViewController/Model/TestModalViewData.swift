8b078147f077f81197e6d8bd66af06d547c9ceb6
Crois0509 <crois0509@naver.com>
2025-01-22 11:14:18 +0900

    ✅[test] 모달뷰 수정 테스트용 더미 데이터 생성

---
+++ TripLog/TripLog/Feat-Crois/ModalViewController/Model/TestModalViewData.swift
@@ -0,0 +1,16 @@
+//
+//  TestModalViewData.swift
+//  TripLog
+//
+//  Created by 장상경 on 1/22/25.
+//
+
+import Foundation
+
+struct TestModalViewData {
+    let isCardPayment: Bool
+    let expenseDetails: String?
+    let category: String?
+    let carrency: Currency
+    let amount: Int?
+}
