ca1ec6414e1aef5b7a7eb94fa158469ab9425a56
Crois0509 <crois0509@naver.com>
2025-01-23 14:35:31 +0900

    ✨[feat] 설정뷰 파일 생성

---
+++ TripLog/TripLog/Feat-Crois/SettingViewController/View/SettingView.swift
@@ -0,0 +1,16 @@
+//
+//  SettingView.swift
+//  TripLog
+//
+//  Created by 장상경 on 1/23/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SettingView: UIView {
+    
+    
+    
+}
