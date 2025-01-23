474bc56b3376ce3448d0c8a407717f8c3c86d780
Crois0509 <crois0509@naver.com>
2025-01-23 13:44:45 +0900

    ✨[feat] 설정 탭 테이블뷰 데이터 소스 구현

--- TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableViewDataSource.swift
+++ TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableViewDataSource.swift
@@ -5,4 +5,49 @@
 //  Created by 장상경 on 1/23/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+struct SetTableSection {
+    var header: String
+    var items: [SettingTableCellModel]
+}
+
+extension SetTableSection: SectionModelType {
+    typealias Item = SettingTableCellModel
+    
+    init(original: SetTableSection, items: [SettingTableCellModel]) {
+        self = original
+        self.items = items
+    }
+}
+
+private extension SettingViewController {
+    typealias DataSource = RxTableViewSectionedReloadDataSource<SetTableSection>
+    
+    var sections: Observable<SetTableSection> {
+        Observable.just(
+            SetTableSection(
+                header: "기본 설정",
+                items: SettingTableCellModel.setTableModels
+            )
+        )
+    }
+    
+    var dataSource: DataSource {
+        let dataSource = DataSource(configureCell: { dataSource, tableView, indexPath, item in
+            // 임시 데이터
+            // 셀 구현 후 내용 변경
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            return cell
+            
+        }, titleForHeaderInSection: { dataSource, index in
+            return dataSource[index].header
+            
+        })
+        
+        return dataSource
+    }
+}
