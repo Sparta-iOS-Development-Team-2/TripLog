f03368787c54a398b79e2d01730cc56dea5ec6cf
Crois0509 <crois0509@naver.com>
2025-01-23 14:29:02 +0900

    ✨[feat] 셀 UI 구현

--- TripLog/TripLog/Feat-Crois/SettingViewController/View/SetTableViewCell.swift
+++ TripLog/TripLog/Feat-Crois/SettingViewController/View/SetTableViewCell.swift
@@ -6,19 +6,34 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class SetTableViewCell: UITableViewCell {
     
     static let id = "SetTableViewCell"
     
-    private let icon = UIImageView()
+    private let icon = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .clear
+    }
     
-    private let title = UILabel()
+    private let title = UILabel().then {
+        $0.font = UIFont.SCDream(size: .display, weight: .medium)
+        $0.numberOfLines = 1
+        $0.textColor = .Dark.base
+        $0.textAlignment = .left
+        $0.backgroundColor = .clear
+    }
     
-    private var extraView: UIView? = nil
+    private var extraView: UIView?
+    
+    private var action: (() -> Void)?
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
@@ -28,7 +43,44 @@ final class SetTableViewCell: UITableViewCell {
     func configureCell(model: SettingTableCellModel) {
         self.icon.image = model.icon
         self.title.text = model.title
-        self.extraView = model.activeView
+        self.extraView = model.extraView
+        self.action = model.action
+    }
+    
+}
+
+private extension SetTableViewCell {
+    
+    func setupUI() {
+        configureSelf()
+        setupLayout()
+    }
+    
+    func configureSelf() {
+        self.backgroundColor = .clear
+        if self.extraView != nil {
+            [icon, title, extraView!].forEach { self.addSubview($0) }
+        } else {
+            [icon, title].forEach { self.addSubview($0) }
+        }
+    }
+   
+    func setupLayout() {
+        icon.snp.makeConstraints {
+            $0.width.height.equalTo(26)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        title.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(icon.snp.trailing).inset(16)
+        }
+        
+        extraView?.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16)
+        }
     }
     
 }
