b24daef198538533bd659a2ed9bbc3c8c1875c73
Crois0509 <crois0509@naver.com>
2025-01-23 15:24:50 +0900

    ♻️[refactor] 데이터소스 리팩토링

--- TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableViewDataSource.swift
+++ TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableViewDataSource.swift
@@ -11,8 +11,10 @@ import RxCocoa
 import RxDataSources
 
 /// 설정 탭의 테이블뷰 섹션을 정의하는 모델
+///
+/// 추후 설정 탭이 늘어났을 때, 주석 처리된 header 옵션을 활성화 시켜 header로 구분 가능
 struct SetTableSection {
-    var header: String
+//    var header: String
     var items: [SettingTableCellModel]
 }
 
@@ -28,32 +30,32 @@ extension SetTableSection: SectionModelType {
 
 // MARK: - SettingViewController DataSource
 
-private extension SettingViewController {
+extension SettingViewController {
     typealias DataSource = RxTableViewSectionedReloadDataSource<SetTableSection>
     
     // 테이블뷰 섹션 정의
-    var sections: Observable<SetTableSection> {
-        Observable.just(
-            SetTableSection(
-                header: "기본 설정",
-                items: SettingTableCellModel.defaultSettingModels
-            )
-        )
+    var sections: BehaviorRelay<[SetTableSection]> {
+        .init(value: [
+                SetTableSection(
+                    // header: "기본 설정",
+                    items: SettingTableCellModel.defaultSettingModels)
+                ]
+              )
     }
     
     // 테이블뷰 데이터소스 정의
     var dataSource: DataSource {
         let dataSource = DataSource(configureCell: { dataSource, tableView, indexPath, item in
+            
             guard let cell = tableView.dequeueReusableCell(withIdentifier: SetTableViewCell.id, for: indexPath) as? SetTableViewCell
-            else { return UITableViewCell() }
+            else { return .init() }
                     
             cell.configureCell(model: item)
             
             return cell
             
-        }, titleForHeaderInSection: { dataSource, index in
-            return dataSource[index].header
-            
+//        }, titleForHeaderInSection: { dataSource, index in
+//            return dataSource[index].header
         })
         
         return dataSource
