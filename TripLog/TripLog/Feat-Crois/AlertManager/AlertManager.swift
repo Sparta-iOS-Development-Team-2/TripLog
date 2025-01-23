2a6f2f40f59f0533e525bffc93935a92fde11c38
Crois0509 <crois0509@naver.com>
2025-01-23 11:46:12 +0900

    💄[style] 문서 주석 추가

--- TripLog/TripLog/Feat-Crois/AlertManager/AlertManager.swift
+++ TripLog/TripLog/Feat-Crois/AlertManager/AlertManager.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+/**
+ 프로젝트 전역에서 사용할 Alert 객체
+ 
+ - **Example**:
+ ```swift
+ // 기본적인 Alert 사용 방법
+ let alert = AlertManager(title: "알림", message: "저장하시겠습니까?", cancelTitle: "취소")
+ 
+ alert.showAlert(on: self, .alert)
+ ```
+ */
 struct AlertManager {
     let title: String
     let message: String
@@ -15,6 +26,24 @@ struct AlertManager {
     let destructiveTitle: String?
     let completion: (() -> Void)?
     
+    // 가장 기본적인 형태의 Alert
+    // cancel 버튼만 보유
+    // handler 없음
+    init(title: String,
+         message: String,
+         cancelTitle: String
+    ) {
+        self.title = title
+        self.message = message
+        self.cancelTitle = cancelTitle
+        self.activeTitle = nil
+        self.destructiveTitle = nil
+        self.completion = nil
+    }
+    
+    // 2개의 선택지를 제공하는 Alert
+    // defaults 형태의 버튼 제공
+    // handler 있음
     init(title: String,
          message: String,
          cancelTitle: String,
@@ -29,6 +58,9 @@ struct AlertManager {
         self.destructiveTitle = nil
     }
     
+    // 2개의 선택지를 제공하는 Alert
+    // destructive 형태의 버튼 제공
+    // handler 있음
     init(title: String,
          message: String,
          cancelTitle: String,
@@ -43,18 +75,10 @@ struct AlertManager {
         self.activeTitle = nil
     }
     
-    init(title: String,
-         message: String,
-         cancelTitle: String
-    ) {
-        self.title = title
-        self.message = message
-        self.cancelTitle = cancelTitle
-        self.activeTitle = nil
-        self.destructiveTitle = nil
-        self.completion = nil
-    }
-    
+    /// Alert 을 뷰 컨트롤러에 present 하는 메소드
+    /// - Parameters:
+    ///   - view: Alert을 present 할 뷰 컨트롤러
+    ///   - style: AlertViewController Style
     func showAlert(on view: UIViewController, _ style: UIAlertController.Style) {
         let alert = UIAlertController(title: self.title, message: self.message, preferredStyle: style)
         alert.addAction(UIAlertAction(title: self.cancelTitle, style: .cancel))
