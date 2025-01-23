8f5bb28804437de854249e9da80da052f0f005c9
Crois0509 <crois0509@naver.com>
2025-01-23 13:44:03 +0900

    ✨[feat] 설정 탭 셀 모델 구현

--- TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableCellModel.swift
+++ TripLog/TripLog/Feat-Crois/SettingViewController/Model/SettingTableCellModel.swift
@@ -6,3 +6,42 @@
 //
 
 import UIKit
+
+struct SettingTableCellModel {
+    private let icon: UIImage
+    private let title: String
+    private let activeView: UIView?
+    private let action: (() -> Void)?
+    
+    static var setTableModels: [SettingTableCellModel] = [
+        SettingTableCellModel(
+            icon: UIImage(named: "darkModeIcon") ?? UIImage(),
+            title: "다크모드",
+            activeView: UISwitch(),
+            action: nil
+        ),
+        
+        SettingTableCellModel(
+            icon: UIImage(named: "reviewIcon") ?? UIImage(),
+            title: "리뷰 작성하기",
+            activeView: UISwitch(),
+            action: nil
+        ),
+        
+        SettingTableCellModel(
+            icon: UIImage(named: "mailIcon") ?? UIImage(),
+            title: "문의하기",
+            activeView: UISwitch(),
+            action: nil
+        ),
+        
+        SettingTableCellModel(
+            icon: UIImage(named: "versionIcon") ?? UIImage(),
+            title: "버전 1.0.0",
+            activeView: UISwitch(),
+            action: nil
+        )
+    ]
+
+}
+
