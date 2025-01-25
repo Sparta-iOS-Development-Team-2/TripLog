769165c1faaaa86faeed9f72c7b4fcb04bb72ebe
Crois0509 <crois0509@naver.com>
2025-01-25 15:52:35 +0900

    ✨[feat] 런치스크린 구현

--- TripLog/TripLog/MainViewController.swift
+++ TripLog/TripLog/MainViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Lottie
+import Then
 
 class MainViewController: UIViewController {
-
+    
+    private lazy var animationView = LottieAnimationView(name: "triplog").then {
+        $0.frame = view.bounds
+        $0.center = view.center
+        $0.alpha = 1
+    }
+    private lazy var image = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .clear
+        $0.image = UIImage(named: "launchImage")
+        $0.frame = view.bounds
+        $0.center = view.center
+        $0.alpha = 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
-
-
+    
 }
 
+private extension MainViewController {
+    
+    func setupUI() {
+        configureSelf()
+        playLottie()
+    }
+    
+    func configureSelf() {
+        [image, animationView].forEach { view.addSubview($0) }
+    }
+    
+    func playLottie() {
+        animationView.loopMode = .repeat(3)
+        animationView.play { _ in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.animationView.alpha = 0
+                self.image.alpha = 0
+            }, completion: { _ in
+                self.animationView.isHidden = true
+                self.image.isHidden = true
+                self.animationView.removeFromSuperview()
+                self.image.removeFromSuperview()
+            })
+        }
+    }
+}
