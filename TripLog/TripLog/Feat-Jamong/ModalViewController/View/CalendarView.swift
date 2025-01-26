166a03d8b8f68060cd76883c5a6d1560d2ec8f25
Jamong-mini <jamong.alsl@gmail.com>
2025-01-26 17:06:13 +0900

    ✨ [feat] FSCalendar 기본 셋업 완료
    구현 상세 목록
     1. FSCalendar 기본 셋업 완료
       * UIView를 상속받은 CalendarView 생성
       * FSCalendar delegate, datasource 설정

--- TripLog/TripLog/Feat-Jamong/ModalViewController/View/CalendarView.swift
+++ TripLog/TripLog/Feat-Jamong/ModalViewController/View/CalendarView.swift
@@ -8,68 +8,29 @@
 
 import UIKit
 import FSCalendar
-import SnapKit
 import Then
+import SnapKit
 
-final class CalendarView: UIView {
+
+final class CalendarView: UIView, FSCalendarDelegate, FSCalendarDataSource {
     // MARK: - Properties
-    weak var delegate: FSCalendarDelegate? {
-        didSet {
-            calendar.delegate = delegate
-        }
-    }
-    
-    private lazy var calendar = FSCalendar().then {
-        // 스크롤 방향 설정
-        $0.scrollDirection = .horizontal
-        
-        // 캘린더 스코프 설정 -> month: 월간, week: 주간
-        $0.scope = .month
-        
-        $0.appearance.do {
-            // 헤더(년월) 포맷 및 스타일 설정
-            $0.headerDateFormat = "YYYY년 MM월"
-            $0.headerTitleColor = .black
-            $0.headerTitleFont = .boldSystemFont(ofSize: 16)
-            $0.headerMinimumDissolvedAlpha = 0.0
-            
-            // 요일 레이블 스타일 설정
-            $0.weekdayFont = .systemFont(ofSize: 14)
-            $0.weekdayTextColor = .gray
-            
-            // 날짜 셀 스타일 설정
-            $0.titleFont = .systemFont(ofSize: 14)
-            $0.titleDefaultColor = .black
-            $0.titleSelectionColor = .white
-            
-            $0.subtitleFont = .systemFont(ofSize: 10)
-            $0.subtitleDefaultColor = .systemGray
-            
-            $0.subtitleOffset = CGPoint(x: 0, y: 2)
-            
-            $0.selectionColor = .systemBlue
-            $0.todayColor = .systemGray4
-            
-            // 이벤트 표시 스타일 설정
-            $0.eventDefaultColor = .systemBlue
-            $0.eventSelectionColor = .white
-        }
+    lazy var calendar = FSCalendar().then {
+        $0.delegate = self
+        $0.dataSource = self
     }
     
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI()
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupUI()
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - UI Setup
     private func setupUI() {
-        backgroundColor = .white
+        backgroundColor = .white    // UI+Extension Color로 설정 필요함.
         addSubview(calendar)
         
         calendar.snp.makeConstraints {
