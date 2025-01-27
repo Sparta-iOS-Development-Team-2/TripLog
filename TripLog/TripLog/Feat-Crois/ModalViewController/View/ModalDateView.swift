5543656b1d21bf08c6232f318cdb60e31bbcfac8
Crois0509 <crois0509@naver.com>
2025-01-27 11:42:37 +0900

    ✨[feat] 모달뷰 데이트뷰 설정 메소드 추가

--- TripLog/TripLog/Feat-Crois/ModalViewController/View/ModalDateView.swift
+++ TripLog/TripLog/Feat-Crois/ModalViewController/View/ModalDateView.swift
@@ -56,6 +56,13 @@ final class ModalDateView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func configureDate(start: Date, end: Date) {
+        self.startDate = start
+        self.endDate = end
+        self.startDatePicker.configureTextField(date: start)
+        self.endDatePicker.configureTextField(date: end)
+    }
 }
 
 // MARK: - UI Setting Method
